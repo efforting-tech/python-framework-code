434fddbb8741f38eca1e9747b539df4fd19c7fba
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2024-08-02 02:06:35 +0200

    Added mnemonic unwrapper

--- testing/text/mnemonic.py
+++ testing/text/mnemonic.py
@@ -34,6 +34,28 @@ from efforting.mvp6.text.styling import presets as SP
 from efforting.mvp6.text.styling.terminal import render_styled_text
 from efforting.mvp6.text import Line_Listing
 from efforting.mvp6.text.tree import Text_Tree_Listing
+from efforting.mvp6.record.base.public import Structure
+from efforting.mvp6.record import member as M
+
+
+
+
+class mnemonic_argument(Structure):
+	name = M.positional()
+	post_processor = M.positional(None)
+
+	def acquire(self, processor_state):
+		if self.post_processor:
+			return self.post_processor(processor_state.captures[self.name])
+		else:
+			return processor_state.captures[self.name]
+
+class mnemonic_unwrapper(Structure):
+	function = M.positional()
+	arguments = M.positional(factory=list)
+
+	def __call__(self, processor_state):
+		return self.function(processor_state, *(arg.acquire(processor_state) for arg in self.arguments))
 
 
 #TODO - move to data utils
@@ -63,28 +85,37 @@ def process_mnemonic(processor_state):
 	names = tuple(cit(mnemonic))
 	assert (len(names) <= 1) or (len(set(names)) == len(names))	#If there are more than one, make sure there are no duplicates
 
-
-	unpack_to = ', '.join(names)
-	unpack_from = ', '.join(f'string_formatter().process_item(processor_state.captures[{n!r}])' for n in names)	#This is now assuming that we do want to turn this to text, which we may not want
 	#TODO - make sure that we know how to handle each of the captures (we will use capture_meta for it)
 	#TODO - we must be able to couple functions and additional code - either by turning the expression into code or by creating unique slots we can access (closure?)
 	#TODO - investigage if we can use closures alltogether (it is possible (see above))
 	#		but we may instead opt to use a wrapper that does the preprocessing before calling the user function
-	unpack_line = f'[{unpack_to}] = [{unpack_from}]'
+	#unpack_line = f'[{unpack_to}] = [{unpack_from}]'
 
 	b = processor_state.node.body.editable_copy()
 	b.normalize_block()
-	b.insert_line(unpack_line)
-	function_def = f'def handler(processor_state):'
+	#b.insert_line(unpack_line)
+
+	args = ', '.join(('processor_state', *names))
+	function_def = f'def handler({args}):'
 	python_code = Text_Tree_Listing.from_title_and_branches(function_def, b).to_str()
 
 	#TODO - use context system
-	gscope = dict(
-		string_formatter = string_formatter,
-	)	#Yes - very ugly (should use context system)
 	scope = dict()
-	exec(python_code, gscope, scope)
-	processor_state.register(mnemonic)(scope['handler'])
+	exec(python_code, scope)
+	mu = mnemonic_unwrapper(scope['handler'])
+
+	for n in names:
+		mu.arguments.append(mnemonic_argument(
+			n,
+			post_processor = lambda n: string_formatter().process_item(n)	#TODO - This is now assuming that we do want to turn this to text, which we may not want (we should rely on capture_meta)
+		))
+
+
+
+
+	#string_formatter().process_item(processor_state.captures[{n!r}])
+
+	processor_state.register(mnemonic)(mu)
 
 
 

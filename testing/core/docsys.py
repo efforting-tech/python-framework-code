1f139f75f6fb338c9b7900d45fa19783cd9259b4
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2024-11-16 20:54:08 +0100

    Made docsys working using a hacky regex translator, still better than all previous attempts

--- testing/core/docsys.py
+++ testing/core/docsys.py
@@ -5,7 +5,6 @@ from efforting.mvp6.core.text.tokenization import Tokenization_Specifier, A
 from efforting.mvp6.core import record as R
 from efforting.mvp6.core.text.tokenization.factory import MVP_Tokenizer_Factory
 
-t = Immutable_Tree_View.from_str(Path('/srv/datacore2/devilholk/Projects/efforting.tech/github/efforting-mvp6/planning/Template Language.tdoc').read_text())
 
 
 #These are records for the things our tokenizer may encounter
@@ -58,51 +57,126 @@ expression.include_tokenizer(innermost_common)
 
 main_tokenizer = MVP_Tokenizer_Factory.implement_tokenizer(top)
 
+import re
 
+
+class MEP_Capture_Data(R.Record):
+	regular_expression: R.Field()
+	name: R.Field() = None
+	post_processor: R.Field() = None
+	validator: R.Field() = None
+
+
+mnemonic_expression_parser = Tree_View_Regex_Dispatcher()
+
+@mnemonic_expression_parser.register_function(r'(?i:name\s+as\s+(\w+))')
+def mep_name_as_alias(dispatcher, node, result):
+	[alias] = result.value.match.groups()
+	return MEP_Capture_Data(rf'(?P<{alias}>\w+)', alias)
+
+@mnemonic_expression_parser.register_function(r'(?i:name)')
+def mep_name(dispatcher, node, result):
+	return MEP_Capture_Data(r'(\w+)')
+
+@mnemonic_expression_parser.register_function(r'(?i:text)')
+def mep_text(dispatcher, node, result):
+	return MEP_Capture_Data(r'(.+)')
+
+def translate_tokens_to_text(item):
+	match item:
+		case list():
+			return ''.join(map(translate_tokens_to_text, item))
+
+		case Word(value) | Text(value):
+			return value
+
+		case Whitespace(value):
+			return value
+
+		case unmatched:
+			raise Exception(unmatched)
+
+def translate_tokens_to_regex(item):
+	match item:
+		case list():
+			return ''.join(map(translate_tokens_to_regex, item))
+
+		case Expression(value): 	#HACK - we will simply translate this to text and manage by a separate matcher
+			#NOTE - we could cache this entire thing
+			text = translate_tokens_to_text(value)
+			mep_values = mnemonic_expression_parser.dispatch_tree(Immutable_Tree_View.from_str(text)).value
+			regex = ''.join(mv.regular_expression for mv in mep_values)
+			return regex
+
+
+		case Word(value) | Text(value):
+			return re.escape(value)
+
+		case Whitespace(value):
+			return r'\s+'
+
+		case Optional(value):
+			return rf'(?:{translate_tokens_to_regex(value)})?'
+
+		case unmatched:
+			raise Exception(unmatched)
+
+
+
+#HACK - this should not be a regex dispatcher but we will use that for now
 class Tree_Dispatcher(Tree_View_Regex_Dispatcher):
-
 	def register_handler(self, pattern):
-		print(main_tokenizer.tokenize(pattern).tokens)
-
+		tokens = main_tokenizer.tokenize(pattern).tokens
+		regex = re.compile(translate_tokens_to_regex(tokens), re.I)
+		return super().register_function(regex)
 
 d = Tree_Dispatcher()
 
 
-#@d.register_function(r'(?i:meta\s+commentary:?)')
-d.register_handler('Meta Commentary[:]')
+@d.register_handler('Meta Commentary[:]')
 def meta_commentary(dispatcher, node, result):
-	#print(dispatcher, text, match)
 	print(result)
 
-#@d.register_function(r'(?i:define\s+software\s+project:?)')
-d.register_handler('Define {name as category} Project[:]')
-def def_sw_p(dispatcher, node, result):
-	#print(dispatcher, text, match)
+@d.register_handler('Define {name as category} Project[:]')
+def define_project_of_category(dispatcher, node, result):
 	(category,) = result.value.match.groups()
 	print(category)
 
-#@d.register_function(r'(?i:sketch:?\s*(.*))')
-d.register_handler('Sketch[:] {name}')
-def def_sw_p(dispatcher, node, result):
+@d.register_handler('Sketch[:] {text}')
+def define_sketch(dispatcher, node, result):
+	(text,) = result.value.match.groups()
+	print(text)
+
+@d.register_handler('Note[:]')
+def define_body_comment(dispatcher, node, result):
+	pass
+
+@d.register_handler('Note[:] {text}')
+def define_line_and_body_comment(dispatcher, node, result):
+	(text,) = result.value.match.groups()
+	print(text)
+
+@d.register_handler('Explanation for {name}[:]')
+def define_explanation(dispatcher, node, result):
 	(name,) = result.value.match.groups()
 	print(name)
 
+@d.register_handler('Language[:] {text}')
+def define_language(dispatcher, node, result):
+	(name,) = result.value.match.groups()
+	print(name)
 
-# @d.register_function(r'(?i:meta\s+commentary:?)')
-# def meta_commentary(dispatcher, node, result):
-# 	#print(dispatcher, text, match)
-# 	print(result)
+@d.register_handler('Directory[:] {text}')
+def define_directory(dispatcher, node, result):
+	(name,) = result.value.match.groups()
+	print(name)
 
-# @d.register_function(r'(?i:define\s+software\s+project:?)')
-# def def_sw_p(dispatcher, node, result):
-# 	#print(dispatcher, text, match)
-# 	print(result)
-
-# @d.register_function(r'(?i:sketch:?\s*(.*))')
-# def def_sw_p(dispatcher, node, result):
-# 	#print(dispatcher, text, match)
-# 	print(result.value.match.groups())
+@d.register_handler('Attachments[:]')
+def define_language(dispatcher, node, result):
+	pass
 
 
 
-#d.dispatch_tree(t)
+
+
+d.dispatch_tree(Immutable_Tree_View.from_str(Path('/srv/datacore2/devilholk/Projects/efforting.tech/github/efforting-mvp6/planning/Template Language.tdoc').read_text()))
